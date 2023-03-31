9aee7bae26a03b3352710dc89999653adfec6b6f
attarmau <87364730+attarmau@users.noreply.github.com>
2023-03-31 15:00:52 +0800

    Update MD5 file hash.py

--- MD5 file hash.py
+++ MD5 file hash.py
@@ -4,5 +4,9 @@ import glob
 from tqdm import tqdm
 
 target = 'here is the name of the MD5 file you want to find'
-
 fileist = [i.replace('.\\', '') for i in glob.glob(os.path.join('./', '*.png'))]
+
+# MD5 file hash
+for filename in tdqm(filelist):
+  md5 = hashlib.mb5()
+  
