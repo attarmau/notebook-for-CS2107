b3be18607387df29b75620fed2a89a5f1b2d8186
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-12 18:35:21 +0800

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,2 +1,5 @@
-    #enter your encrypted message(string) below
+cdef decrypt():
+    
+#enter your encrypted message(string) below
     encrypted_message = input("Gurer_V5_n_YbG_Bs_Uby3f_Va_Gu1f_Ba3 ").strip()
+ letters="abcdefghijklmnopqrstuvwxyz"
