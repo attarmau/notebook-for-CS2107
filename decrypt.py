e9da7e3b1f89162003f5f6ef29271e2bdcfdc6c8
attarmau <87364730+attarmau@users.noreply.github.com>
2023-04-16 21:05:03 +0800

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -4,3 +4,7 @@ cdef decrypt():
     encrypted_message = input("Gurer_V5_n_YbG_Bs_Uby3f_Va_Gu1f_Ba3 ").strip()
     
     letters="abcdefghijklmnopqrstuvwxyz"
+    
+    #enter the key value to decrypt
+    k = int(input("Gurer_V5_n_YbG_Bs_Uby3f_Va_Gu1f_Ba3"))
+    decrypted_message = ""
