79c2d24abd0adeb785275f5f923f268e0e34ddff
attarmau <87364730+attarmau@users.noreply.github.com>
2023-06-18 11:44:49 +0800

    Create password checker.py

---
+++ password checker.py
@@ -0,0 +1,24 @@
+# Question: "You have a text file containing a list of passwords that needs to be checked for their strength. Implement a Python program that reads the file and determines the strength of each password based on the following criteria:
+
+- The password must be at least 8 characters long.
+- It must contain a combination of letters (both uppercase and lowercase), numbers, and special characters (!, @, #, $, %, ^, &, *).
+- It should not contain common words or patterns commonly used in passwords (such as "password", "12345678", "qwerty", etc.).
+- The program should output the strength of each password as 'Weak', 'Medium', or 'Strong'."
+
+import re
+def check_password_strength(password):
+    # Minimum length check
+    if len(password) < 8:
+        return 'Weak'
+    # Character combination check
+    if not re.search(r"[a-z]", password) or not re.search(r"[A-Z]", password) or \
+            not re.search(r"\d", password) or not re.search(r"[!@#$%^&*]", password):
+        return 'Weak'
+
+    # Common words or patterns check
+    common_patterns = ['password', '12345678', 'qwerty']
+    for pattern in common_patterns:
+        if pattern in password:
+            return 'Weak'
+
+    return 'Strong'
