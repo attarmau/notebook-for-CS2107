8469bd49df735fbeed1abce194e209101dd0c1d5
attarmau <87364730+attarmau@users.noreply.github.com>
2023-09-16 08:28:23 +0800

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -12,4 +12,23 @@ def caesar_cipher_decrypt(encrypted_message, key):
             decrypted_message += ch
 
     return decrypted_message
+def caesar_cipher_encrypt(message, key):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    encrypted_message = ""
 
+    for ch in message:
+        if ch in letters:
+            position = letters.find(ch)
+            new_pos = (position + key) % 26
+            new_char = letters[new_pos]
+            encrypted_message += new_char
+        else:
+            encrypted_message += ch
+
+    return encrypted_message
+
+# Example usage:
+message = input("Enter the message to encrypt: ").strip()
+key = int(input("Enter the key: "))
+encrypted_message = caesar_cipher_encrypt(message, key)
+print("Encrypted message:", encrypted_message)
