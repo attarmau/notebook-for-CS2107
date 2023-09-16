48319e8095c2ec19e335c4c27167af7eb76cc06a
attarmau <87364730+attarmau@users.noreply.github.com>
2023-09-16 08:27:18 +0800

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,4 +1,23 @@
+def caesar_cipher_decrypt(encrypted_message, key):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    decrypted_message = ""
 
+    for ch in encrypted_message:
+        if ch in letters:
+            position = letters.find(ch)
+            new_pos = (position - key) % 26
+            new_char = letters[new_pos]
+            decrypted_message += new_char
+        else:
+            decrypted_message += ch
+
+    return decrypted_message
+
+# Example usage:
+encrypted_message = input("Enter the encrypted message: ").strip()
+key = int(input("Enter the key: "))
+decrypted_message = caesar_cipher_decrypt(encrypted_message, key)
+print("Decrypted message:", decrypted_message)
 
 # Given a string 'message' and a secret key 'key', write a Python function to encrypt the message using the Caesar cipher
 
